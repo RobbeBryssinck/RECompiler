b2a61da245f7538345389fb841aef58776e0b6ba
Robbe Bryssinck <robbebryssinck@gmail.com>
2023-05-10 17:23:40 +0200

    RELauncher tests

--- Tests/RELauncher_Tests/RELauncherTest.cpp
+++ Tests/RELauncher_Tests/RELauncherTest.cpp
@@ -1,9 +1,24 @@
 #include <gtest/gtest.h>
 #include <RELauncher/RELauncher.h>
+#include <filesystem>
 
 namespace
 {
-  TEST(RELauncherSettings, TestLoadExistingSettings)
+  class ScopedSampleDir : public ::testing::Test
+  {
+  protected:
+    void SetUp() override
+    {
+      std::filesystem::current_path(std::filesystem::current_path() / "LaunchSample");
+    }
+
+    void TearDown() override
+    {
+      std::filesystem::current_path(std::filesystem::current_path().parent_path());
+    }
+  };
+
+  TEST_F(ScopedSampleDir, LoadExistingSettings)
   {
     RELauncher::Settings settings{};
     
@@ -12,32 +27,53 @@ namespace
     EXPECT_EQ(result, RELauncher::Settings::LoadResult::kOk);
   }
 
-  class LaunchFixture : public ::testing::Test
+  class SettingsFixture : public ScopedSampleDir
   {
-  public:
-    static void SetUpTestSuite()
-    {
-      settings.LoadSettings();
-    }
-
-    static RELauncher::Settings settings;
-
   protected:
     void SetUp() override
     {
+      ScopedSampleDir::SetUp();
+      settings.LoadSettings();
+    }
+
+    void TearDown() override
+    {
+      ScopedSampleDir::TearDown();
+    }
+
+    RELauncher::Settings settings;
+  };
+
+  TEST_F(SettingsFixture, LaunchSampleProject)
+  {
+    RELauncher::LaunchInfo info = RELauncher::Launch(settings);
+
+    EXPECT_EQ(info.result, RELauncher::LaunchResult::kOk);
+
+    // TODO: exit code?
+    TerminateProcess(info.process, 1);
+    CloseHandle(info.process);
+  }
+
+  class LaunchFixture : public SettingsFixture
+  {
+  protected:
+    void SetUp() override
+    {
+      SettingsFixture::SetUp();
       info = RELauncher::Launch(settings);
     }
 
     void TearDown() override
     {
+      SettingsFixture::TearDown();
       // TODO: exit code?
       TerminateProcess(info.process, 1);
+      CloseHandle(info.process);
     }
 
     RELauncher::LaunchInfo info;
   };
-
-  RELauncher::Settings LaunchFixture::settings = RELauncher::Settings();
 
   TEST_F(LaunchFixture, LaunchFromSettings)
   {
